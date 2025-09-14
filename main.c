5a67abe8ba766e82c5e4e99fac2a3938082923d3
Valeria Pombo <valepombo22@gmail.com>
2025-09-14 18:29:37 -0300

    formateo con clang-format

--- main.c
+++ main.c
@@ -1,70 +1,70 @@
+#include "tp1.h"
 #include <stdio.h>
 #include <string.h>
-#include "tp1.h"
-
 
 bool mostrar_pokemon(struct pokemon *pk, void *extra) {
-    printf("%d - %s (%d,%d,%d)\n",
-           pk->id, pk->nombre, pk->ataque, pk->defensa, pk->velocidad);
-    return true;
+  printf("%d - %s (%d,%d,%d)\n", pk->id, pk->nombre, pk->ataque, pk->defensa,
+         pk->velocidad);
+  return true;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Uso: %s <archivo1.csv> <operacion> [archivo2.csv] [resultado.csv]\n", argv[0]);
-        return 1;
+  if (argc < 3) {
+    printf(
+        "Uso: %s <archivo1.csv> <operacion> [archivo2.csv] [resultado.csv]\n",
+        argv[0]);
+    return 1;
+  }
+
+  const char *archivo1 = argv[1];
+  const char *operacion = argv[2];
+
+  tp1_t *tp1 = tp1_leer_archivo(archivo1);
+  if (!tp1) {
+    printf("Error al leer archivo %s\n", archivo1);
+    return 1;
+  }
+
+  if (strcmp(operacion, "mostrar") == 0) {
+
+    tp1_con_cada_pokemon(tp1, mostrar_pokemon, NULL);
+  } else if (argc >= 5) {
+    const char *archivo2 = argv[3];
+    const char *resultado = argv[4];
+
+    tp1_t *tp2 = tp1_leer_archivo(archivo2);
+    if (!tp2) {
+      printf("Error al leer archivo %s\n", archivo2);
+      tp1_destruir(tp1);
+      return 1;
     }
 
-    const char *archivo1 = argv[1];
-    const char *operacion = argv[2];
+    tp1_t *res = NULL;
 
-    tp1_t *tp1 = tp1_leer_archivo(archivo1);
-    if (!tp1) {
-        printf("Error al leer archivo %s\n", archivo1);
-        return 1;
+    if (strcmp(operacion, "union") == 0)
+      res = tp1_union(tp1, tp2);
+    else if (strcmp(operacion, "interseccion") == 0)
+      res = tp1_interseccion(tp1, tp2);
+    else if (strcmp(operacion, "diferencia") == 0)
+      res = tp1_diferencia(tp1, tp2);
+    else {
+      printf("Operacion desconocida\n");
+      tp1_destruir(tp1);
+      tp1_destruir(tp2);
+      return 1;
     }
 
-    if (strcmp(operacion, "mostrar") == 0) {
- 
-        tp1_con_cada_pokemon(tp1, mostrar_pokemon, NULL);
-    } 
-    else if (argc >= 5) {
-        const char *archivo2 = argv[3];
-        const char *resultado = argv[4];
-
-        tp1_t *tp2 = tp1_leer_archivo(archivo2);
-        if (!tp2) {
-            printf("Error al leer archivo %s\n", archivo2);
-            tp1_destruir(tp1);
-            return 1;
-        }
-
-        tp1_t *res = NULL;
-
-        if (strcmp(operacion, "union") == 0)
-            res = tp1_union(tp1, tp2);
-        else if (strcmp(operacion, "interseccion") == 0)
-            res = tp1_interseccion(tp1, tp2);
-        else if (strcmp(operacion, "diferencia") == 0)
-            res = tp1_diferencia(tp1, tp2);
-        else {
-            printf("Operacion desconocida\n");
-            tp1_destruir(tp1);
-            tp1_destruir(tp2);
-            return 1;
-        }
-
-        if (res) {
-            tp1_guardar_archivo(res, resultado);
-            printf("Operacion %s realizada, guardada en %s\n", operacion, resultado);
-            tp1_destruir(res);
-        }
-
-        tp1_destruir(tp2);
-    } else {
-        printf("Parametros insuficientes para operacion %s\n", operacion);
+    if (res) {
+      tp1_guardar_archivo(res, resultado);
+      printf("Operacion %s realizada, guardada en %s\n", operacion, resultado);
+      tp1_destruir(res);
     }
 
-    tp1_destruir(tp1);
-    return 0;
+    tp1_destruir(tp2);
+  } else {
+    printf("Parametros insuficientes para operacion %s\n", operacion);
+  }
+
+  tp1_destruir(tp1);
+  return 0;
 }
--- src/tp1.c
+++ src/tp1.c
@@ -1,347 +1,381 @@
+#include "tp1.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include "tp1.h"
 
 struct tp1 {
-    struct pokemon *pokemones;
-    size_t cantidad; 
-    size_t capacidad;
+  struct pokemon *pokemones;
+  size_t cantidad;
+  size_t capacidad;
 };
 
-
 static enum tipo_pokemon tipo_desde_string(const char *str) {
-    if(strcmp(str, "ELEC") == 0) return TIPO_ELEC;
-    if(strcmp(str, "FUEG") == 0) return TIPO_FUEG;
-    if(strcmp(str, "PLAN") == 0) return TIPO_PLAN;
-    if (strcmp(str, "AGUA") == 0) return TIPO_AGUA;
-    if (strcmp(str, "NORM") == 0) return TIPO_NORM;
-    if (strcmp(str, "FANT") == 0) return TIPO_FANT;
-    if (strcmp(str, "PSI") == 0) return TIPO_PSI;
-    if (strcmp(str, "LUCH") == 0) return TIPO_LUCH;
+  if (strcmp(str, "ELEC") == 0)
+    return TIPO_ELEC;
+  if (strcmp(str, "FUEG") == 0)
+    return TIPO_FUEG;
+  if (strcmp(str, "PLAN") == 0)
+    return TIPO_PLAN;
+  if (strcmp(str, "AGUA") == 0)
+    return TIPO_AGUA;
+  if (strcmp(str, "NORM") == 0)
     return TIPO_NORM;
+  if (strcmp(str, "FANT") == 0)
+    return TIPO_FANT;
+  if (strcmp(str, "PSI") == 0)
+    return TIPO_PSI;
+  if (strcmp(str, "LUCH") == 0)
+    return TIPO_LUCH;
+  return TIPO_NORM;
 }
 
 static bool existe_id(tp1_t *tp, int id) {
-    size_t i;
-    for (i=0; i < tp->cantidad; i++) {
-        if(tp->pokemones[i].id == id) return true;
-    }
-    return false; 
+  size_t i;
+  for (i = 0; i < tp->cantidad; i++) {
+    if (tp->pokemones[i].id == id)
+      return true;
+  }
+  return false;
 }
 
 size_t tp1_cantidad(tp1_t *tp) {
-    if (!tp) return 0;
-    return tp->cantidad;
+  if (!tp)
+    return 0;
+  return tp->cantidad;
 }
 
-
-
 tp1_t *tp1_leer_archivo(const char *nombre_archivo) {
-    FILE *f = fopen(nombre_archivo, "r");
-    if (!f) return NULL;
+  FILE *f = fopen(nombre_archivo, "r");
+  if (!f)
+    return NULL;
 
-    tp1_t *tp = malloc(sizeof(tp1_t));
-    if(!tp) {
-        fclose(f);
-        return NULL;
-    }
-
-    tp->pokemones = NULL;
-    tp->cantidad = 0;
-    tp->capacidad = 0;
-
-    char linea[256];
-
-    while (fgets(linea, sizeof(linea), f)) {
-        int id, ataque, defensa, velocidad;
-        char nombre[256];
-        char tipo_str[16];
-
-        int leidos = sscanf(linea, "%d,%255[^,],%15[^,],%d,%d,%d", &id, nombre, tipo_str, &ataque, &defensa, &velocidad);
-
-        bool valido = (leidos == 6) && !existe_id(tp, id);
-
-        if (valido) {
-            if (tp->cantidad == tp->capacidad) {
-                size_t nueva_capacidad = (tp->capacidad == 0) ? 4 : tp->capacidad * 2;
-                struct pokemon *nuevo_array = realloc(tp->pokemones, nueva_capacidad * sizeof(struct pokemon));
-                if(!nuevo_array) {
-                    tp1_destruir(tp);
-                    fclose(f);
-                    return NULL;
-                }
-                tp->pokemones = nuevo_array;
-                tp->capacidad = nueva_capacidad;
-            }
-
-            nombre[strcspn(nombre, "\r\n")] = '\0';
-            tipo_str[strcspn(tipo_str, "\r\n")] = '\0';
-
-            struct pokemon *pk = &tp->pokemones[tp->cantidad];
-            pk->id = id;
-            pk->nombre = strdup(nombre);
-            pk->tipo = tipo_desde_string(tipo_str);
-            pk->ataque = ataque;
-            pk->defensa = defensa;
-            pk->velocidad = velocidad; 
-
-            tp->cantidad++;
-        }
-    }
-
+  tp1_t *tp = malloc(sizeof(tp1_t));
+  if (!tp) {
     fclose(f);
-    return tp;
+    return NULL;
+  }
+
+  tp->pokemones = NULL;
+  tp->cantidad = 0;
+  tp->capacidad = 0;
+
+  char linea[256];
+
+  while (fgets(linea, sizeof(linea), f)) {
+    int id, ataque, defensa, velocidad;
+    char nombre[256];
+    char tipo_str[16];
+
+    int leidos = sscanf(linea, "%d,%255[^,],%15[^,],%d,%d,%d", &id, nombre,
+                        tipo_str, &ataque, &defensa, &velocidad);
+
+    bool valido = (leidos == 6) && !existe_id(tp, id);
+
+    if (valido) {
+      if (tp->cantidad == tp->capacidad) {
+        size_t nueva_capacidad = (tp->capacidad == 0) ? 4 : tp->capacidad * 2;
+        struct pokemon *nuevo_array =
+            realloc(tp->pokemones, nueva_capacidad * sizeof(struct pokemon));
+        if (!nuevo_array) {
+          tp1_destruir(tp);
+          fclose(f);
+          return NULL;
+        }
+        tp->pokemones = nuevo_array;
+        tp->capacidad = nueva_capacidad;
+      }
+
+      nombre[strcspn(nombre, "\r\n")] = '\0';
+      tipo_str[strcspn(tipo_str, "\r\n")] = '\0';
+
+      struct pokemon *pk = &tp->pokemones[tp->cantidad];
+      pk->id = id;
+      pk->nombre = strdup(nombre);
+      pk->tipo = tipo_desde_string(tipo_str);
+      pk->ataque = ataque;
+      pk->defensa = defensa;
+      pk->velocidad = velocidad;
+
+      tp->cantidad++;
+    }
+  }
+
+  fclose(f);
+  return tp;
 }
 
 tp1_t *tp1_guardar_archivo(tp1_t *tp, const char *nombre_archivo) {
-    if (!tp || !nombre_archivo) return NULL;
+  if (!tp || !nombre_archivo)
+    return NULL;
 
-    FILE *f = fopen(nombre_archivo, "w");
-    if (!f) return NULL;
+  FILE *f = fopen(nombre_archivo, "w");
+  if (!f)
+    return NULL;
 
-    for (size_t i = 0; i < tp->cantidad; i++) {
-        struct pokemon *p = &tp->pokemones[i];
+  for (size_t i = 0; i < tp->cantidad; i++) {
+    struct pokemon *p = &tp->pokemones[i];
 
-        const char *tipo_str;
-        if (p->tipo == TIPO_ELEC) tipo_str = "ELEC";
-        else if (p->tipo == TIPO_FUEG) tipo_str = "FUEG";
-        else if (p->tipo == TIPO_PLAN) tipo_str = "PLAN";
-        else if (p->tipo == TIPO_AGUA) tipo_str = "AGUA";
-        else if (p->tipo == TIPO_NORM) tipo_str = "NORM";
-        else if (p->tipo == TIPO_FANT) tipo_str = "FANT";
-        else if (p->tipo == TIPO_PSI)  tipo_str = "PSI";
-        else if (p->tipo == TIPO_LUCH) tipo_str = "LUCH";
-        else tipo_str = "NORM";
+    const char *tipo_str;
+    if (p->tipo == TIPO_ELEC)
+      tipo_str = "ELEC";
+    else if (p->tipo == TIPO_FUEG)
+      tipo_str = "FUEG";
+    else if (p->tipo == TIPO_PLAN)
+      tipo_str = "PLAN";
+    else if (p->tipo == TIPO_AGUA)
+      tipo_str = "AGUA";
+    else if (p->tipo == TIPO_NORM)
+      tipo_str = "NORM";
+    else if (p->tipo == TIPO_FANT)
+      tipo_str = "FANT";
+    else if (p->tipo == TIPO_PSI)
+      tipo_str = "PSI";
+    else if (p->tipo == TIPO_LUCH)
+      tipo_str = "LUCH";
+    else
+      tipo_str = "NORM";
 
-        fprintf(f, "%d,%s,%s,%d,%d,%d\n",
-                p->id, p->nombre, tipo_str, p->ataque, p->defensa, p->velocidad);
-    }
+    fprintf(f, "%d,%s,%s,%d,%d,%d\n", p->id, p->nombre, tipo_str, p->ataque,
+            p->defensa, p->velocidad);
+  }
 
-    fclose(f);
-    return tp;
+  fclose(f);
+  return tp;
 }
 
-
 struct pokemon *tp1_buscar_nombre(tp1_t *tp, const char *nombre) {
-    if (!tp || !nombre) return NULL;
-
-    size_t i;
-    for (i=0; i < tp->cantidad; i++){
-        if (strcmp(tp->pokemones[i].nombre, nombre) == 0)
-            return &tp->pokemones[i];
-    }
-
+  if (!tp || !nombre)
     return NULL;
+
+  size_t i;
+  for (i = 0; i < tp->cantidad; i++) {
+    if (strcmp(tp->pokemones[i].nombre, nombre) == 0)
+      return &tp->pokemones[i];
+  }
+
+  return NULL;
 }
 
 struct pokemon *tp1_buscar_id(tp1_t *tp, int id) {
-    if(!tp) return NULL;
-
-    size_t i;
-
-    for (i=0; i < tp->cantidad; i++) {
-        if(tp->pokemones[i].id == id)
-            return &tp->pokemones[i];
-    }
-
+  if (!tp)
     return NULL;
+
+  size_t i;
+
+  for (i = 0; i < tp->cantidad; i++) {
+    if (tp->pokemones[i].id == id)
+      return &tp->pokemones[i];
+  }
+
+  return NULL;
 }
 
 tp1_t *tp1_union(tp1_t *un_tp, tp1_t *otro_tp) {
-    if (!un_tp || !otro_tp) return NULL;
+  if (!un_tp || !otro_tp)
+    return NULL;
 
-    tp1_t *nuevo = malloc(sizeof(tp1_t));
-    if (!nuevo) return NULL;
+  tp1_t *nuevo = malloc(sizeof(tp1_t));
+  if (!nuevo)
+    return NULL;
 
-    nuevo->pokemones = NULL;
-    nuevo->cantidad = 0;
-    nuevo->capacidad = 0;
+  nuevo->pokemones = NULL;
+  nuevo->cantidad = 0;
+  nuevo->capacidad = 0;
 
-    size_t i;
-    for (i=0; i < un_tp->cantidad; i++) {
-        struct pokemon *pk = &un_tp->pokemones[i];
+  size_t i;
+  for (i = 0; i < un_tp->cantidad; i++) {
+    struct pokemon *pk = &un_tp->pokemones[i];
 
-        if(nuevo->cantidad == nuevo->capacidad) {
-            size_t nueva_cap = (nuevo->capacidad == 0) ? 4 : nuevo->capacidad * 2;
-            struct pokemon *aux = realloc(nuevo->pokemones, nueva_cap * sizeof(struct pokemon)); 
-            if(!aux) {
+    if (nuevo->cantidad == nuevo->capacidad) {
+      size_t nueva_cap = (nuevo->capacidad == 0) ? 4 : nuevo->capacidad * 2;
+      struct pokemon *aux =
+          realloc(nuevo->pokemones, nueva_cap * sizeof(struct pokemon));
+      if (!aux) {
 
-                tp1_destruir(nuevo);
-                return NULL;
-            }
-            nuevo->pokemones = aux;
-            nuevo->capacidad = nueva_cap; 
-        }
-
-        struct pokemon *p = &nuevo->pokemones[nuevo->cantidad];
-        p->id = pk->id;
-        p->nombre = strdup(pk->nombre);
-        p->tipo = pk->tipo;
-        p->ataque = pk->ataque;
-        p->defensa = pk->defensa;
-        p->velocidad = pk->velocidad;
-        
-        nuevo->cantidad++;
+        tp1_destruir(nuevo);
+        return NULL;
+      }
+      nuevo->pokemones = aux;
+      nuevo->capacidad = nueva_cap;
     }
 
-    for (i=0; i< otro_tp->cantidad; i++) {
-        struct pokemon *pk = &otro_tp->pokemones[i]; 
+    struct pokemon *p = &nuevo->pokemones[nuevo->cantidad];
+    p->id = pk->id;
+    p->nombre = strdup(pk->nombre);
+    p->tipo = pk->tipo;
+    p->ataque = pk->ataque;
+    p->defensa = pk->defensa;
+    p->velocidad = pk->velocidad;
 
-        bool existe = false; 
-        size_t j;
-        for(j=0; j < nuevo->cantidad; j++) {
-            if (nuevo->pokemones[j].id == pk->id) {
-                existe = true; 
-            }
-        }
+    nuevo->cantidad++;
+  }
 
-        if (!existe) {
-            if (nuevo->cantidad == nuevo->capacidad) {
-                size_t nueva_cap = (nuevo->capacidad == 0) ? 4 : nuevo->capacidad * 2;
-                struct pokemon *aux = realloc(nuevo->pokemones, nueva_cap * sizeof(struct pokemon));
-                if (!aux) {
-                    tp1_destruir(nuevo);
-                    return NULL;
-                }
-                nuevo->pokemones = aux;
-                nuevo->capacidad = nueva_cap;
-            }
+  for (i = 0; i < otro_tp->cantidad; i++) {
+    struct pokemon *pk = &otro_tp->pokemones[i];
 
-            struct pokemon *p = &nuevo->pokemones[nuevo->cantidad]; 
-            p->id = pk->id;
-            p->nombre = strdup(pk->nombre);
-            p->tipo = pk->tipo;
-            p->ataque = pk->ataque;
-            p->defensa = pk->defensa; 
-            p->velocidad = pk->velocidad;
-
-            nuevo->cantidad++;
-        }
+    bool existe = false;
+    size_t j;
+    for (j = 0; j < nuevo->cantidad; j++) {
+      if (nuevo->pokemones[j].id == pk->id) {
+        existe = true;
+      }
     }
 
-    return nuevo;
+    if (!existe) {
+      if (nuevo->cantidad == nuevo->capacidad) {
+        size_t nueva_cap = (nuevo->capacidad == 0) ? 4 : nuevo->capacidad * 2;
+        struct pokemon *aux =
+            realloc(nuevo->pokemones, nueva_cap * sizeof(struct pokemon));
+        if (!aux) {
+          tp1_destruir(nuevo);
+          return NULL;
+        }
+        nuevo->pokemones = aux;
+        nuevo->capacidad = nueva_cap;
+      }
+
+      struct pokemon *p = &nuevo->pokemones[nuevo->cantidad];
+      p->id = pk->id;
+      p->nombre = strdup(pk->nombre);
+      p->tipo = pk->tipo;
+      p->ataque = pk->ataque;
+      p->defensa = pk->defensa;
+      p->velocidad = pk->velocidad;
+
+      nuevo->cantidad++;
+    }
+  }
+
+  return nuevo;
 }
 
 tp1_t *tp1_interseccion(tp1_t *un_tp, tp1_t *otro_tp) {
-    if (!un_tp || !otro_tp) return NULL;
+  if (!un_tp || !otro_tp)
+    return NULL;
 
-    tp1_t *nuevo = malloc(sizeof(tp1_t)); 
-    if (!nuevo) return NULL;
+  tp1_t *nuevo = malloc(sizeof(tp1_t));
+  if (!nuevo)
+    return NULL;
 
-    nuevo->pokemones = NULL;
-    nuevo->cantidad = 0;
-    nuevo->capacidad = 0; 
+  nuevo->pokemones = NULL;
+  nuevo->cantidad = 0;
+  nuevo->capacidad = 0;
 
-    size_t i;
-    for (i=0; i < un_tp->cantidad; i++) {
-        struct pokemon *pk = &un_tp->pokemones[i];
+  size_t i;
+  for (i = 0; i < un_tp->cantidad; i++) {
+    struct pokemon *pk = &un_tp->pokemones[i];
 
-        bool existe = false;
-        size_t j;
-        for (j=0; j < otro_tp->cantidad; j++) {
-            if(pk->id == otro_tp->pokemones[j].id) {
-                existe = true;
-            }
-        }
-
-        if (existe) {
-            if (nuevo->cantidad == nuevo->capacidad) {
-                size_t nueva_cap = (nuevo->capacidad == 0) ? 4 : nuevo->capacidad * 2;
-                struct pokemon *aux = realloc(nuevo->pokemones, nueva_cap * sizeof(struct pokemon));
-                if (!aux) {
-                    tp1_destruir(nuevo);
-                    return NULL;
-                } 
-                nuevo->pokemones = aux;
-                nuevo->capacidad = nueva_cap;
-            }
-
-            struct pokemon *p = &nuevo->pokemones[nuevo->cantidad];
-            p->id = pk->id;
-            p->nombre = strdup(pk->nombre);
-            p->tipo = pk->tipo;
-            p->ataque = pk->ataque;
-            p->defensa = pk->defensa;
-            p->velocidad = pk->velocidad;
-
-            nuevo->cantidad++;
-        }
+    bool existe = false;
+    size_t j;
+    for (j = 0; j < otro_tp->cantidad; j++) {
+      if (pk->id == otro_tp->pokemones[j].id) {
+        existe = true;
+      }
     }
 
-    return nuevo;
+    if (existe) {
+      if (nuevo->cantidad == nuevo->capacidad) {
+        size_t nueva_cap = (nuevo->capacidad == 0) ? 4 : nuevo->capacidad * 2;
+        struct pokemon *aux =
+            realloc(nuevo->pokemones, nueva_cap * sizeof(struct pokemon));
+        if (!aux) {
+          tp1_destruir(nuevo);
+          return NULL;
+        }
+        nuevo->pokemones = aux;
+        nuevo->capacidad = nueva_cap;
+      }
+
+      struct pokemon *p = &nuevo->pokemones[nuevo->cantidad];
+      p->id = pk->id;
+      p->nombre = strdup(pk->nombre);
+      p->tipo = pk->tipo;
+      p->ataque = pk->ataque;
+      p->defensa = pk->defensa;
+      p->velocidad = pk->velocidad;
+
+      nuevo->cantidad++;
+    }
+  }
+
+  return nuevo;
 }
 
 tp1_t *tp1_diferencia(tp1_t *un_tp, tp1_t *otro_tp) {
-    if (!un_tp || !otro_tp) return NULL;
+  if (!un_tp || !otro_tp)
+    return NULL;
 
-    tp1_t *nuevo = malloc(sizeof(tp1_t));
-    if(!nuevo) return NULL;
+  tp1_t *nuevo = malloc(sizeof(tp1_t));
+  if (!nuevo)
+    return NULL;
 
-    nuevo->pokemones = NULL;
-    nuevo->cantidad = 0;
-    nuevo->capacidad = 0;
+  nuevo->pokemones = NULL;
+  nuevo->cantidad = 0;
+  nuevo->capacidad = 0;
 
-    size_t i;
-    for(i=0; i < un_tp->cantidad; i++) {
-        struct pokemon *pk = &un_tp->pokemones[i];
+  size_t i;
+  for (i = 0; i < un_tp->cantidad; i++) {
+    struct pokemon *pk = &un_tp->pokemones[i];
 
-        bool existe = false;
-        size_t j;
-        for (j=0; j < otro_tp->cantidad; j++) {
-            if(pk->id == otro_tp->pokemones[j].id) {
-                existe = true;
-            }
-        }
-
-        if (!existe) {
-            if(nuevo->cantidad == nuevo->capacidad) {
-                size_t nueva_cap = (nuevo->capacidad == 0) ? 4 : nuevo->capacidad * 2; 
-                struct pokemon *aux = realloc(nuevo->pokemones, nueva_cap * sizeof(struct pokemon));
-                if (!aux) {
-                    tp1_destruir(nuevo);
-                    return NULL;
-                }
-                nuevo->pokemones = aux;
-                nuevo->capacidad = nueva_cap; 
-        }
-
-        struct pokemon *p = &nuevo->pokemones[nuevo->cantidad]; 
-        p->id = pk->id;
-        p->nombre = strdup(pk->nombre);
-        p->tipo = pk->tipo;
-        p->ataque = pk->ataque;
-        p->defensa = pk->defensa;
-        p->velocidad = pk->velocidad;
-
-        nuevo->cantidad++;
+    bool existe = false;
+    size_t j;
+    for (j = 0; j < otro_tp->cantidad; j++) {
+      if (pk->id == otro_tp->pokemones[j].id) {
+        existe = true;
+      }
     }
 
-}
-    return nuevo;
-}
-
-size_t tp1_con_cada_pokemon(tp1_t *tp, bool (*f)(struct pokemon *, void *), void *extra) {
-    if (!tp || !f) return 0;
-
-    size_t cont=0;
-    size_t i;
-    for (i=0; i < tp->cantidad; i++) {
-        if(!f(&tp->pokemones[i], extra)) {
-            return cont;
+    if (!existe) {
+      if (nuevo->cantidad == nuevo->capacidad) {
+        size_t nueva_cap = (nuevo->capacidad == 0) ? 4 : nuevo->capacidad * 2;
+        struct pokemon *aux =
+            realloc(nuevo->pokemones, nueva_cap * sizeof(struct pokemon));
+        if (!aux) {
+          tp1_destruir(nuevo);
+          return NULL;
         }
-        cont++;
+        nuevo->pokemones = aux;
+        nuevo->capacidad = nueva_cap;
+      }
+
+      struct pokemon *p = &nuevo->pokemones[nuevo->cantidad];
+      p->id = pk->id;
+      p->nombre = strdup(pk->nombre);
+      p->tipo = pk->tipo;
+      p->ataque = pk->ataque;
+      p->defensa = pk->defensa;
+      p->velocidad = pk->velocidad;
+
+      nuevo->cantidad++;
     }
-    return cont;
+  }
+  return nuevo;
+}
+
+size_t tp1_con_cada_pokemon(tp1_t *tp, bool (*f)(struct pokemon *, void *),
+                            void *extra) {
+  if (!tp || !f)
+    return 0;
+
+  size_t cont = 0;
+  size_t i;
+  for (i = 0; i < tp->cantidad; i++) {
+    if (!f(&tp->pokemones[i], extra)) {
+      return cont;
+    }
+    cont++;
+  }
+  return cont;
 }
 
 void tp1_destruir(tp1_t *tp) {
-    if(!tp) return;
-    size_t i;
-    for (i=0; i < tp->cantidad; i++) {
-        free(tp->pokemones[i].nombre);
-    }
-    free(tp->pokemones);
-    free(tp);
+  if (!tp)
+    return;
+  size_t i;
+  for (i = 0; i < tp->cantidad; i++) {
+    free(tp->pokemones[i].nombre);
+  }
+  free(tp->pokemones);
+  free(tp);
 }
